bf583d817a0f153343b3c2204239441a8177e2a0
Sebastian Gibaja <trigocristopher@gmail.com>
2023-02-12 17:25:07 +0100

    hasta el minuto 1:26:00

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,44 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Yuta");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("ignorado@hotmail.com");
+        usuario.setTelefono("123412341");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario123")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Yuta");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("ignorado@hotmail.com");
+        usuario.setTelefono("123412341");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario321")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Yuta");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("ignorado@hotmail.com");
+        usuario.setTelefono("123412341");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario132")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Yuta");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("ignorado@hotmail.com");
+        usuario.setTelefono("123412341");
+        return usuario;
     }
 }
