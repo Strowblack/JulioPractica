cf6b30c58ab539795500760e4c613798b046fd16
Sebastian Gibaja <trigocristopher@gmail.com>
2023-02-12 20:01:55 +0100

    hasta el minuto 2:25:00

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.dao.UsuarioDao;
 import com.s2daw.demo.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class UsuarioController {
+public class UsuarioController{
+    @Autowired
+    private UsuarioDao usuarioDao;
 
     @RequestMapping(value="usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
@@ -24,32 +28,7 @@ public class UsuarioController {
 
     @RequestMapping(value="usuarios")
     public List<Usuario> getUsuarios(){
-        List<Usuario> usuarios= new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(234L);
-        usuario.setNombre("Yuta");
-        usuario.setApellido("Aoi");
-        usuario.setEmail("ignorado@hotmail.com");
-        usuario.setTelefono("123412341");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(123L);
-        usuario2.setNombre("Hinata");
-        usuario2.setApellido("Aoi");
-        usuario2.setEmail("noignorado@hotmail.com");
-        usuario2.setTelefono("4812774");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(142L);
-        usuario3.setNombre("Selene Inmaculada");
-        usuario3.setApellido("García Chacón");
-        usuario3.setEmail("enana@cute.com");
-        usuario3.setTelefono("81200");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
     @RequestMapping(value="usuario123")
@@ -80,5 +59,9 @@ public class UsuarioController {
         usuario.setEmail("ignorado@hotmail.com");
         usuario.setTelefono("123412341");
         return usuario;
+    }
+
+    public List<Usuario> patito() {
+        return null;
     }
 }
